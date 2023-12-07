884200f7f1c3c6476660f132b02ce558564a9ce3
Chamika Jayasinghe <hirosanajaya2001@gmail.com>
2023-12-07 10:41:58 +0530

    added the seeder.js file to destroy and populate the db + script

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,19 +2,84 @@ const fs = require('fs')
 const TourModel = require('../models/tourModel')
 
 
+// @desc --> create a tour
+// POST / api / v1 / tours
+const createTour = async (req, res) => {
+
+    // we should write async /await functions inside a try/catch block
+
+    try {
+        // here create method returns a promise so we can use async await to handle it
+        const newTour = await TourModel.create(req.body)
+
+        res.status(201)
+            .json({
+                status: "Successful",
+                message: "Tour created successfully"
+            })
+
+    } catch (error) {
+
+        res.status(400).json({
+            status: "Unsuccessful",
+            msg: error.message
+        })
+
+    }
+}
 
 
 // @desc --> Get all tours
+// GET /api/v1/tours
+const getAllTours = async (req, res) => {
 
-const getAllTours = (req, res) => { }
+    try {
 
+        const allTours = await TourModel.find()
 
+        res.status(200)
+            .json({
+                status: "successful",
+                totalTours: allTours.length,
+                allTours: allTours
+            })
 
+    } catch (error) {
+
+        res.status(404).json({
+            status: "Unsuccessful",
+            msg: error.message
+
+        })
+
+    }
+
+}
 
 
 // @desc --> Get one tour
+// GET /api/v1/tours/:id
+const getOneTour = async (req, res) => {
 
-const getOneTour = (req, res) => {
+    try {
+
+        const askedTour = await TourModel.findById(req.params.id)  // to access the parameters use req.params not req.body 😊
+
+        res.status(200)
+            .json({
+                status: "successful",
+                tour: askedTour
+            })
+
+    } catch (error) {
+
+        res.status(404)
+            .json({
+                status: "Unsuccessful",
+                errorMsg: error.message
+            })
+
+    }
 
 
 
@@ -25,30 +90,62 @@ const getOneTour = (req, res) => {
 
 
 // @desc --> Update a tour
+// PATCH /api/v1/tours/:id
 
-const updateTour = (req, res) => {
+const updateTour = async (req, res) => {
+
+    try {
+
+        // findByIdAndUpdate --> refer mongoose docs
+        const updatedTour = await TourModel.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { returnDocument: 'after', runValidators: true }
+        )
 
 
 
+        res.status(200)
+            .json({
+                status: "successfully updated",
+                updatedTour: updatedTour
+
+            })
 
 
 
+    } catch (error) {
+
+        res.status(404).json({
+            status: "unsuccessful",
+            errMsg: error.message
+        })
+    }
 
 }
 
-// @desc --> Create a tour 
 
-const createTour = (req, res) => {
+// @desc --> delete a tour 
+// DELETE /api/v1/tours/:id
+const deleteTour = async (req, res) => {
+
+    try {
+
+        await TourModel.findByIdAndDelete(req.params.id)
+
+        res.status(204).json({ status: "successful deletion" })
+
+    } catch (error) {
+
+        res.status(404)
+            .json({
+                status: "unsuccessful",
+                errMsg: error.message
+            })
+    }
+
 }
 
-
-const deleteTour = (req, res) => {
-
-
-
-
-
-}
 
 module.exports = {
     getAllTours,
--- models/tourModel.js
+++ models/tourModel.js
@@ -20,21 +20,67 @@ const tourSchema = new mongoose.Schema({
 
     },
 
-    rating: {
+    ratingsAverage: {
         type: Number,
-        default: 0.0
+        default: 0
 
+    },
+
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
 
     difficulty: {
         type: String,
+        required: [true, 'Difficulty must be given'],
         enum: {
             values: ['easy', 'medium', 'difficult'],
             message: 'Tour difficulty must be --> easy/ medium/ difficult '
         }
+    },
 
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration'],
+        min: [1, 'Tour must be at least 1 day long'],
+        max: [30, 'Max duration is 30 days']
+    },
 
-    }
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'Group size must be mentioned']
+
+    },
+
+    priceDiscount: Number,
+
+    summary: {
+        type: String,
+        required: [true, 'A summary must be given about the tour'],
+        trim: true,
+
+    },
+
+    description: {
+        type: String,
+        minlength: [20, 'description length >= 20 char']
+
+    },
+
+    imageCover: {
+        type: String, // we will store the path to the image in the db, not the image itself
+        required: [true, 'Must have a cover image']
+    },
+
+    images: [String],
+
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+
+    startDates: [Date]
 
 })
 
